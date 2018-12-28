dacde180714900a9dd796ffeb542e9761194dc2a
soartec-lab <info@soartec-lab.work>
2018-12-29 00:37:28 +0900

    create def scheme_create

--- app/models/elasticsearch/base.rb
+++ app/models/elasticsearch/base.rb
@@ -1,10 +1,11 @@
 class Elasticsearch::Base
-  attr_reader :client, :index
+  attr_reader :client, :index, :type
 
   def initialize
     config = Rails.application.config.elasticsearch
     connection_url = "#{config["host"]}:#{config["port"]}"
     @index = config["index"]
+    @type = self.class.name.tableize
     @client = Elasticsearch::Client.new url: connection_url
   end
 
@@ -22,5 +23,30 @@ class Elasticsearch::Base
     client.index index: index, type: type, body: attributes
   end
 
+  def self.scheme_create
+    object = new
+    client = object.client
+    index = object.index
+    type = object.type
 
+    body = {
+      mappings: {
+        "#{type}" => {
+          properties: {
+            title: {
+              type: 'text',
+              analyzer: "kuromoji"
+            },
+            description: {
+              type: 'text',
+              analyzer: "kuromoji"
+            }
+          }
+        }
+      }
+    }
+    
+    client.indices.create index: index, body: body
+  end
 end
+
