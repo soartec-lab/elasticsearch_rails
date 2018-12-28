5f71b14290603019db1a545aeb223d539ae8cd5f
soartec-lab <info@soartec-lab.work>
2018-12-29 00:02:53 +0900

    add def create

--- app/models/elasticsearch/base.rb
+++ app/models/elasticsearch/base.rb
@@ -1,7 +1,21 @@
 class Elasticsearch::Base
+  attr_reader :client, :index
+
   def initialize
     config = Rails.application.config.elasticsearch
     connection_url = "#{config["host"]}:#{config["port"]}"
+    @index = config["index"]
     @client = Elasticsearch::Client.new url: connection_url
   end
+
+  def self.create(attributes = nil)
+    object = new
+    client = object.client
+    index = object.index
+    type = name.tableize
+
+    client.index index: index, type: type, body: attributes
+  end
+
+
 end
